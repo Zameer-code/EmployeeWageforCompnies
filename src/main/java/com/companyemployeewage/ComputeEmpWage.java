67f556e8c5857978e83b2898f09ac14c35e5c122
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-11 17:28:44 +0530

    Delete src directory

--- src/main/java/com/companyemployeewage/ComputeEmpWage.java
+++
@@ -1,8 +0,0 @@
-package com.companyemployeewage;
-
-public interface ComputeEmpWage {
-
-	 public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkDays,int maxHoursMonth);
-	    public void computeEmpWage();
-	    
-}
--- src/main/java/com/companyemployeewage/Main.java
+++
@@ -1,7 +0,0 @@
-package com.companyemployeewage;
-
-public class Main {
-	 public static void main(String[] args) {
-			
-		    }
-}
